d2eb16d30e3cb09355e9da3bff817832500331bc
luckyboy-wqx <wuqingxin2008@gmail.com>
2021-05-10 23:15:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const WebpackDevMiddleware=require('webpack-dev-middleware');
+const WebpackHotMiddleware=require('webpack-hot-middleware');
+const Webpack=require('webpack');
+
+exports.plugin={
+	name:"hapi-webpack-middleware",
+	version:"1.0.0",
+	pkg:require('./package.json'),
+	register:async function(server,options){
+		const config = options.config;
+	    const compiler = Webpack(config);
+	    server.app.webpackCompiler = compiler;
+	    const devMiddleware = WebpackDevMiddleware(compiler, options.options);
+	    const hotMiddleware = WebpackHotMiddleware(compiler, options.options);
+
+	    server.ext('onRequest', (request, reply) => {
+	        const req = request.raw.req;
+	        const res = request.raw.res;
+
+	        devMiddleware(req, res, (err) => {
+
+	            if (err) {
+	                return reply(err);
+	            }
+
+	            return reply.continue();
+	        });
+	    });
+
+	    server.ext('onRequest', (request, reply) => {
+
+	        const req = request.raw.req;
+	        const res = request.raw.res;
+
+	        hotMiddleware(req, res, (err) => {
+
+	            if (err) {
+	                return reply(err);
+	            }
+
+	            return reply.continue();
+	        });
+	    });
+
+	}
+}
